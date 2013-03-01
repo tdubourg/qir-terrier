d546af8cbc5890ad4045837a91db6e1ee085bc07
tdubourg <plop@plop.fake>
2013-03-01 14:57:05 +0100

    WIP on the document vector set and document vector. Switched from simple scalar to pairs (document vector entry) now

--- src/core/org/terrier/structures/DocumentVector.java
+++ src/core/org/terrier/structures/DocumentVector.java
@@ -1,13 +1,43 @@
 package org.terrier.structures;
 
-public class DocumentVector {
-	public void reset() {
+import java.util.ArrayList;
 
+public class DocumentVector {
+
+	public class DocumentVectorEntry {
+		private int termId;
+		private int occurences;
+
+		public DocumentVectorEntry(int termid, int occ) {
+			termId = termid;
+			occurences = occ;
+		}
+
+		public int getTermId() { return termId; }
+		public int getOccurences() { return occurences; }
 	}
+	
+	private int i_pointer;
+	private ArrayList<DocumentVectorEntry> entries;
+
+    public DocumentVector(){
+		entries = new ArrayList<>(100);
+	}
+
+	public void reset() {
+		i_pointer = -1;
+	}
+
 	public boolean endOfVector() {
-		return false;
+		return i_pointer >= entries.size();
 	}
-	public int getNextScalar() {
-		return -1;
+
+	public DocumentVectorEntry getNextEntry() {
+		i_pointer++;
+		if (!endOfVector()) {
+			return entries.get(i_pointer);
+		} else {
+			return null;
+		}
 	}
 }
--- src/core/org/terrier/structures/DocumentVectorSet.java
+++ src/core/org/terrier/structures/DocumentVectorSet.java
@@ -1,14 +1,28 @@
 package org.terrier.structures;
 
-public class DocumentVectorSet {
-	public void reset() {
+import java.util.ArrayList;
 
+public class DocumentVectorSet {
+	private int i_pointer;
+	private ArrayList<DocumentVector> vectors;
+
+	public DocumentVectorSet(){
+		vectors = new ArrayList<>(100);
+	}
+
+	public void reset() {
+		i_pointer = -1;
 	}
 	public boolean endOfVectorSet() {
-		return false;
+		return i_pointer >= vectors.size();
 	}
 	public DocumentVector getNextVector() {
-		return null;
+		i_pointer++;
+		if (!endOfVectorSet()) {
+			return vectors.get(i_pointer);
+		} else {
+			return null;
+		}
 	}
 
     public void insert(DocumentVector pushTerm) {
